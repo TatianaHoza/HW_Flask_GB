e15ee5863e22cc817b91440144539b9894a19eee
TatianaHoza <tatiana-15@tut.by>
2024-01-11 23:58:26 +0300

    change HW_2

--- HW_2/app_task2.py
+++ HW_2/app_task2.py
@@ -3,24 +3,26 @@ from flask import Flask,request,render_template,make_response,redirect,url_for,s
 app = Flask(__name__)
 app.secret_key = '5f214cacbd30c2ae4784b520f17912ae0d5d8c16ae98128e3f549546221265e4'
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    if 'name' in session:
-        return f'Привет, {session["name"]}'
-        #return redirect(url_for('hello.html'))
-    else:
-        return redirect(url_for('submit'))
-    response = make_response("Cookie установлен")
-    response.set_cookie('username', 'admin')
-    return response
+    if request.method == 'POST':
+        if (name := request.form.get('name')) and (email := request.form.get('e-mail')):
+            response = make_response(redirect(url_for('submit')))
+            response.set_cookie('username', name)
+            response.set_cookie('email', email)
+            return response
+    return render_template('base.html')
 
 
 @app.route('/submit/', methods = ['GET','POST'])
 def submit():
     if request.method == 'POST':
-        session['name'] = request.form.get('name') or 'NoName'
-        return redirect(url_for('index'))
-    return render_template('base.html')
+        response = make_response(redirect(url_for('index')))
+        response.delete_cookie('username')
+        response.delete_cookie('email')
+        return response
+    username = request.cookies.get('username')
+    return render_template('hello.html', name=username)
 
 @app.route('/logout/')
 def logout():
